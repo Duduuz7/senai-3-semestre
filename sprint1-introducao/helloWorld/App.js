493223636083f61fe8623f02c6d94a1c72456d9c
Eduardo-Felipe <edufeli2005@gmail.com>
2024-01-25 17:24:31 -0300

    Modificações Hello World inspirado Vital

--- sprint1-introducao/helloWorld/App.js
+++ sprint1-introducao/helloWorld/App.js
@@ -1,14 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, Image, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={[styles.card, styles.shadowProp]}>
-        <Text style={[styles.heading]}>
-          Hello World !!!
-        </Text>
+
+      <Image
+        style={styles.logo}
+        source={require('./src/assets/VitalHub_Logo.png')}
+      />
+
+      <Text style={[styles.title]}>
+        Entrar ou criar conta
+      </Text>
+
+      <TextInput
+        style={styles.input}
+        defaultValue='exemplo de imput'
+      />
+      <TextInput
+        style={styles.input}
+        defaultValue='exemplo de imput'
+      />
+
+      <View style={styles.containerButton}>
+        <TouchableOpacity
+          style={styles.button}>
+          <Text style={styles.textButton}>Entrar</Text>
+        </TouchableOpacity>
       </View>
+
+
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -19,11 +42,12 @@ const styles = StyleSheet.create({
     padding: 20,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'white'
+    backgroundColor: 'white',
+    gap: 10,
   },
-  heading: {
-    color: 'purple',
-    fontSize: 30,
+  title: {
+    color: 'black',
+    fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 13,
   },
@@ -37,9 +61,38 @@ const styles = StyleSheet.create({
   },
   shadowProp: {
     shadowColor: 'purple',
-    shadowOffset: {width: -2, height: 4},
+    shadowOffset: { width: -2, height: 4 },
     shadowOpacity: 0.2,
     shadowRadius: 3,
     elevation: 50
+  },
+  input: {
+    color: '#49B3BA',
+    width: '95%',
+    heith: 53,
+    borderWidth: 2,
+    borderColor: '#49B3BA',
+    borderRadius: 5,
+    padding: 10,
+    marginTop: 10
+  },
+  button: {
+    alignItems: 'center',
+    height: 43,
+    backgroundColor: '#496BBA',
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#496BBA',
+    borderRadius: 5
+  },
+  containerButton: {
+    width: '95%',
+    height: 43,
+    // borderWidth: 1,
+    borderColor: '#496BBA',
+    borderRadius: 5
+  },
+  textButton: {
+    color: 'white',
   },
 });
